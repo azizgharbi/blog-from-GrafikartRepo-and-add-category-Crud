ace84a61ea33f1deb30a9437a9c1621855c40a90
aziz <a.gharbi@emobiletechnologies.com>
2017-02-24 15:16:50 -0500

    loading..

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -24,7 +24,7 @@
                     <td>{{ $category->created_at->format('d/m/Y H:i') }}</td>
                     <td>
                         <a href="{{ route('admin.categories.edit', ['id' => $category->id]) }}" class="btn btn-primary">Edit</a>
-                        {!! Form::model($category, ['method' => 'DELETE', 'route' => ['admin.categories.destroy',$category], 'style' => 'display:inline;','class'=>'delete_form']) !!}
+                        {!! Form::model($category, ['method' => 'DELETE', 'route' => ['admin.categories.destroy',$category], 'style' => 'display:inline;']) !!}
                             <button type="button" class="btn btn-danger">Delete</button>
                         {!! Form::close() !!}
                     </td>
@@ -34,39 +34,6 @@
     </table>
 
     <div class="navigation">
-        {{ $categories->links('vendor.pagination.bootstrap-4') }}
+        {{ $categories->links('vendor.default') }}
     </div>
-@endsection
-@section('js')
-<script type="text/javascript">
- $(document).ready(function () {
- $('.btn-danger').on('click', function(e){
-       e.preventDefault();
-       var self = $(this);
-       swal({
-           title             : "Are you sure?",
-           text              : "You will not be able to recover this category !",
-           type              : "warning",
-           showCancelButton  : true,
-           confirmButtonColor: "#DD6B55",
-           confirmButtonText : "Yes, delete it!",
-           cancelButtonText  : "No, Cancel delete!",
-           closeOnConfirm    : false,
-           closeOnCancel     : false
-       },
-       function(isConfirm){
-           if(isConfirm){
-               swal("Deleted!","this category has been deleted", "success");
-               setTimeout(function() {
-                   self.parents(".delete_form").submit();
-               }, 2000); //2 second delay (2000 milliseconds = 2 seconds)
-           }
-           else{
-                 swal("cancelled","this certification is safe", "error");
-           }
-       });
-   });
-
-});
-</script>
 @endsection
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -26,7 +26,7 @@
                     <td>{{ $post->created_at->format('d/m/Y H:i') }}</td>
                     <td>
                         <a href="{{ route('admin.posts.edit', ['id' => $post->id]) }}" class="btn btn-primary">Edit</a>
-                        {!! Form::model($post, ['method' => 'DELETE', 'route' => ['admin.posts.destroy', $post], 'style' => 'display:inline;','class'=>'delete_form']) !!}
+                        {!! Form::model($post, ['method' => 'DELETE', 'route' => ['admin.posts.destroy', $post], 'style' => 'display:inline;']) !!}
                            {{ csrf_field() }}
                             <button type="button" class="btn btn-danger">Delete</button>
                         {!! Form::close() !!}
@@ -37,39 +37,6 @@
     </table>
 
     <div class="navigation">
-        {{ $posts->links('vendor.pagination.bootstrap-4') }}
+        {{ $posts->links('vendor.pagination.default') }}
     </div>
-@endsection
-@section('js')
-<script type="text/javascript">
- $(document).ready(function () {
- $('.btn-danger').on('click', function(e){
-       e.preventDefault();
-       var self = $(this);
-       swal({
-           title             : "Are you sure?",
-           text              : "You will not be able to recover this post !",
-           type              : "warning",
-           showCancelButton  : true,
-           confirmButtonColor: "#DD6B55",
-           confirmButtonText : "Yes, delete it!",
-           cancelButtonText  : "No, Cancel delete!",
-           closeOnConfirm    : false,
-           closeOnCancel     : false
-       },
-       function(isConfirm){
-           if(isConfirm){
-               swal("Deleted!","this post has been deleted", "success");
-               setTimeout(function() {
-                   self.parents(".delete_form").submit();
-               }, 2000); //2 second delay (2000 milliseconds = 2 seconds)
-           }
-           else{
-                 swal("cancelled","this post is safe", "error");
-           }
-       });
-   });
-
-});
-</script>
 @endsection
