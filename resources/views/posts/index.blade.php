bdf3e5cb50174d46f65e75a3b83334b6418ddbac
aziz <a.gharbi@emobiletechnologies.com>
2017-02-26 18:57:00 -0500

    just test

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -19,7 +19,7 @@
         <p>
             <small>
                 Category : <a href="{{ route('posts.category', ['slug' => $post->category->slug]) }}">{{ $post->category->name }}</a>
-                by <a href="{{ route('post.user', ['id' => $post->user->id]) }}">{{ $post->user->name }}</a>
+                by <a href="{{ route('posts.user', ['id' => $post->user->id]) }}">{{ $post->user->name }}</a>
                 on {{ $post->created_at->format('M dS Y') }}
             </small>
         </p>
